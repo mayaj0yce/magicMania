3ca6e7e71f0300993500da6777aa304f91368654
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-09-25 17:58:44 -0600

    updating app.js with routes to landing page and preparing for other routes

--- magicmania/src/App.js
+++ magicmania/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from "./components/pages/landingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
