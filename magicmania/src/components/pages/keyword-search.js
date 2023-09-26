ebe2d171a6f0df93cac7b0bb452a9bd509c56334
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-09-25 20:49:34 -0600

    added keyword-search page, along with corresponding route

---
+++ magicmania/src/components/pages/keyword-search.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function KeywordSearch() {
+  // State to store the keyword entered by the user
+  const [keyword, setKeyword] = useState('');
+
+  const [showConfirmMessage, setshowConfirmMessage] = useState(false);
+  
+  // Function to handle keyword input changes
+  const handleKeywordChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  
+  // Function to handle keyword search
+  const handleKeywordSearch = (e) => {
+    e.preventDefault();
+    // Add logic for fetching keyword data here
+    console.log(`Searching for cards with keyword: ${keyword}`);
+    setshowConfirmMessage(true);
+  };
+
+  return (
+    <div>
+      {/* Header */}
+
+      {/* Search Form */}
+      <main>
+        <h2>Search to understand the world of Magic!</h2>
+        <form onSubmit={handleKeywordSearch}>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter a keyword"
+              value={keyword}
+              onChange={handleKeywordChange}
+            />
+            <button type="submit">Search</button>
+          </div>
+       </form>
+      
+
+        {/* Display Search Results */}
+        {/* Implement code to display search results here and delete following line */}
+        {showConfirmMessage && <p>Showing results for {keyword}</p>}
+      </main>
+    </div>
+  );
+}
+
+export default KeywordSearch;
