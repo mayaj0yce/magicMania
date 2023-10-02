bdc84b2c4df742dbfb14af745cec106de70461e0
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-10-01 22:27:30 -0600

    modified Resolver for user signup, login, and logout routes

--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -1,63 +1,76 @@
 const User = require('../../db/models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express'); // Import AuthenticationError from the GraphQL server library
 
 const resolvers = {
-    Mutation: {
-        createUser: async (_, { input }) => {
-            const { username, email, password } = input;
-            // Hash the user's password before saving it to the database
-            const hashedPassword = await bcrypt.hash(password, 10);
-      
-            const newUser = new User({
-              username,
-              email,
-              passwordHash: hashedPassword,
-            
-            });
+  Mutation: {
+    createUser: async (_, { input }) => {
+      const { username, email, password } = input;
 
-            const savedUser = await newUser.save();
+      // Hash the user's password before saving it to the database
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-            const token = generateToken(savedUser);
+      try {
+        const newUser = new User({
+          username,
+          email,
+          passwordHash: hashedPassword,
+        });
 
-            return {
-                user: savedUser,
-                token,
-            };
-    },
-    login: async (_, { username, password }) => {
-        const user = await User.findOne({ username });
+        const savedUser = await newUser.save();
 
-        if (!user) {
-            throw new Error('Incorrect passsword<insert Evil laugh>');
-        }
-
-        const token = generateToken(user);
+        const token = generateToken(savedUser);
 
         return {
-            user,
-            token,
+          user: savedUser,
+          token,
         };
+      } catch (error) {
+        // Handle any database or validation errors here
+        throw new Error('Error creating user');
+      }
     },
-    Query: {
-        getUser: (_, { id }) => {
-            return User.findbyId(id);
-        },
-        // moreeeeee?
+    login: async (_, { username, password }) => {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        throw new AuthenticationError('User not found');
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+
+      if (!passwordMatch) {
+        throw new AuthenticationError('Incorrect password');
+      }
+
+      const token = generateToken(user);
+
+      return {
+        user,
+        token,
+      };
     },
-},
+  },
+  Query: {
+    getUser: (_, { id }) => {
+      return User.findById(id);
+    },
+    // Add more queries here if needed
+  },
 };
 
 function generateToken(user) {
-    const token = jwt.sign(
-        {
-            id: user._id,
-            username: user.username,
-        },
-        'superrrrsecret',
-        { expiresIn: '1h'}
-    );
-    return token;
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+      // Add more user data to the token payload if needed
+    },
+    process.env.JWT_SECRET, // Use an environment variable for the secret key
+    { expiresIn: '1h' }
+  );
+  return token;
 }
 
 module.exports = resolvers;
