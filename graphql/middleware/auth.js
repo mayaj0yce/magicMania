f9b4c18250a09ff66aee3f420c65e314086f1358
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-10-03 22:29:40 -0600

    connecting front end to backed through apollo header and refactored login

--- graphql/middleware/auth.js
+++ graphql/middleware/auth.js
@@ -3,16 +3,16 @@ const { User } = require('../../db/models/user');
 
 // Middleware function to verify JWT tokens
 async function authenticateToken(req, res, next) {
-  // Extract the token from the request headers
-  const token = req.header('Authorization');
+  // Extract the token from the request headers (use the same name as in the frontend)
+  const authToken = req.header('Authorization');
 
-  if (!token) {
+  if (!authToken) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
 
   try {
     // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(authToken.replace('Bearer ', ''), process.env.JWT_SECRET);
 
     // Check if the token is associated with an existing user
     const user = await User.findById(decoded.id);
@@ -20,6 +20,8 @@ async function authenticateToken(req, res, next) {
     if (!user) {
       return res.status(401).json({ message: 'Invalid token. User not found.' });
     }
+    
+    // Attach user information to the request
     req.user = user;
     next();
   } catch (error) {
--- magicmania/src/apolloClient.js
+++ magicmania/src/apolloClient.js
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import GetUser from './utils/auth'
 
 // Create an HTTP link to your GraphQL server
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql', // Set the server endpoint to your GraphQL server
+  uri: 'http://localhost:3001/graphql', // Set your server's GraphQL endpoint
+  headers: {
+    Authorization: `Bearer ${GetUser.getToken()}`, // Get the token from storage
+  },
 });
 
 // Create an Apollo Client
